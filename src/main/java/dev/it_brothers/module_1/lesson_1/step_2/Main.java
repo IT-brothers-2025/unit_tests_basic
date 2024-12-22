5166d60d60e2d09fa5b7cc3266182c3ae5fcf1d8
jibulani <evgeni-trofimo@yandex.ru>
2024-12-23 01:33:05 +0300

    add project structure

---
+++ src/main/java/dev/it_brothers/module_1/lesson_1/step_2/Main.java
@@ -0,0 +1,9 @@
+package dev.it_brothers.module_1.lesson_1.step_2;
+
+public class Main {
+
+    // пример кода, который нужно покрыть тестом в рамках шага
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
